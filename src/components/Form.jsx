96a080554172c4712269ef52cc2504485a76e33f
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-09-13 17:24:03 +0200

    framer motion

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,48 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Form = () => {
+  const fadeInAnimation = {
+    initial: {
+      opacity: 0,
+      x: -100,
+    },
+    animate: (seconds) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: seconds,
+      },
+    }),
+  };
+
+  const fadeInAnimationRight = {
+    initial: {
+      opacity: 0,
+      x: 100,
+    },
+    animate: (seconds) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: seconds,
+      },
+    }),
+  };
+
+  const fadeInAnimationUp = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (seconds) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: seconds,
+      },
+    }),
+  };
   return (
     <div name="form" className="pt-12 lg:pt-0 xl:h-screen">
       <div className="w-full my-18 sm:py-8 md:py-6 lg:py-28 flex justify-center items-center p-4">
@@ -9,33 +51,57 @@ const Form = () => {
           action="https://getform.io/f/c37b66e7-3ea0-4d89-b54c-d206b9ed8b5c"
           className="flex flex-col max-w-[600px] w-full"
         >
-          <div className="pb-8">
+          <motion.div
+            variants={fadeInAnimation}
+            initial="initial"
+            whileInView="animate"
+            custom={0.2}
+            className="pb-8"
+          >
             <p className="my-4 text-white font-bold text-3xl border-b-4 w-44 border-sky-800 sm:w-32 hover:text-4xl hover:w-48 duration-500">
               Contact
             </p>
             <p className="text-xl text-gray-400">Submit the form below</p>
-          </div>
-          <input
+          </motion.div>
+          <motion.input
+            variants={fadeInAnimationRight}
+            initial="initial"
+            whileInView="animate"
+            custom={0.3}
             className="bg-[#ccd6f6] p-2"
             type="text"
             placeholder="Name"
             name="name"
           />
-          <input
+          <motion.input
+            variants={fadeInAnimationRight}
+            initial="initial"
+            whileInView="animate"
+            custom={0.4}
             className="my-4 p-2 bg-[#ccd6f6]"
             type="email"
             placeholder="Email"
             name="email"
           />
-          <textarea
+          <motion.textarea
+            variants={fadeInAnimationRight}
+            initial="initial"
+            whileInView="animate"
+            custom={0.5}
             className="bg-[#ccd6f6] p-2"
             name="message"
             rows="10"
             placeholder="Message"
-          ></textarea>
-          <button className="text-white animate-bounce border-2 border-sky-800 hover:bg-sky-800 hover:translate-y-[-0.5rem] duration-500 px-4 py-3 my-8 mx-auto flex items-center">
+          ></motion.textarea>
+          <motion.button
+            variants={fadeInAnimationUp}
+            initial="initial"
+            whileInView="animate"
+            custom={0.6}
+            className="text-white animate-bounce border-2 border-sky-800 hover:bg-sky-800 hover:translate-y-[-0.5rem] duration-500 px-4 py-3 my-8 mx-auto flex items-center"
+          >
             Write Me
-          </button>
+          </motion.button>
         </form>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import Me from "../assets/imgs/me3.jpg";
 import Cv from "./Cv";
-//xs:py-44 sm:py-44 md:py-36
+import { motion } from "framer-motion";
+
 const Hero = () => {
+  const fadeInAnimation = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (seconds) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: seconds,
+      },
+    }),
+  };
+
   return (
     <div name="hero" className="pt-2 lg:pt-10 xl:h-screen">
       <div className="w-full mt-32 md:mt-20 lg:mt-32 xl:mt-48">
@@ -10,11 +25,36 @@ const Hero = () => {
           <div className="w-full grid lg:grid-cols-2">
             <div className="flex justify-center">
               <div>
-                <p className="mx-4 text-sky-800">Hi, my name is</p>
-                <h1 className="mx-4 text-2xl">Cujba Tiberiu Stefan</h1>
-                <h2 className="mx-4 text-2xl text-gray-400">
+                <motion.p
+                  variants={fadeInAnimation}
+                  initial="initial"
+                  whileInView="animate"
+                  custom={0.1}
+                  // viewport={{ once: true }}
+                  className="mx-4 text-sky-800"
+                >
+                  Hi, my name is
+                </motion.p>
+                <motion.h1
+                  variants={fadeInAnimation}
+                  initial="initial"
+                  whileInView="animate"
+                  // viewport={{ once: true }}
+                  custom={0.2}
+                  className="mx-4 text-2xl"
+                >
+                  Cujba Tiberiu Stefan
+                </motion.h1>
+                <motion.h2
+                  variants={fadeInAnimation}
+                  initial="initial"
+                  whileInView="animate"
+                  // viewport={{ once: true }}
+                  custom={0.3}
+                  className="mx-4 text-2xl text-gray-400"
+                >
                   I'm a Full Stack Developer
-                </h2>
+                </motion.h2>
               </div>
             </div>
           </div>
@@ -22,42 +62,116 @@ const Hero = () => {
             <div className="flex justify-center">
               <div>
                 <div className="mx-4">
-                  <p className="my-4 font-bold text-3xl border-b-4 w-40 border-sky-800  hover:text-4xl hover:w-60 duration-500 ">
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0.5}
+                    className="my-4 font-bold text-3xl border-b-4 w-40 border-sky-800  hover:text-4xl hover:w-60 duration-500 "
+                  >
                     Backround
-                  </p>
+                  </motion.p>
                 </div>
                 <div className="mx-4 my-2 gap-4 hover:text-sky-800">
-                  <p>January 2023 - August 2023</p>
-                  <p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0.6}
+                  >
+                    January 2023 - August 2023
+                  </motion.p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0.7}
+                  >
                     Full Stack Course at{" "}
-                    <a
+                    <motion.a
+                      variants={fadeInAnimation}
+                      initial="initial"
+                      whileInView="animate"
+                      custom={0.7}
                       className="hover:text-fuchsia-800"
                       href="https://epicode.com/it/?utm_source=adwords&utm_campaign=Brand&utm_adgroup=brandexact&utm_term=epicode&utm_medium=ppc&hsa_acc=1246633295&hsa_cam=11897141170&hsa_grp=115607542236&hsa_ad=639941169967&hsa_src=g&hsa_tgt=kwd-1083842420863&hsa_kw=epicode&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gclid=CjwKCAjwq4imBhBQEiwA9Nx1Bi8lRPf7RQTdeyeOoQ44guNWS_CWH_Ti-pr_aJdjz6qSvRfYd9xtmhoCpL4QAvD_BwE"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
                       Epicode
-                    </a>
-                  </p>
+                    </motion.a>
+                  </motion.p>
                 </div>
                 <div className="mx-4 my-2 gap-4 hover:text-sky-800">
-                  <p>December 2020 - December 2022</p>
-                  <p>Depot Squad Leader</p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0.8}
+                  >
+                    December 2020 - December 2022
+                  </motion.p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0.9}
+                  >
+                    Depot Squad Leader
+                  </motion.p>
                 </div>
                 <div className="mx-4 my-2 gap-4 hover:text-sky-800">
-                  <p>March 2016 - December 2020</p>
-                  <p>Depot Picker</p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={1}
+                  >
+                    March 2016 - December 2020
+                  </motion.p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={1.1}
+                  >
+                    Depot Picker
+                  </motion.p>
                 </div>
                 <div className="mx-4 my-2 gap-4 hover:text-sky-800">
-                  <p>January 2011 - July 2016</p>
-                  <p>Economics & Bussines High School</p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={1.2}
+                  >
+                    January 2011 - July 2016
+                  </motion.p>
+                  <motion.p
+                    variants={fadeInAnimation}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={1.3}
+                  >
+                    Economics & Bussines High School
+                  </motion.p>
                 </div>
-                <div>
+                <motion.div
+                  variants={fadeInAnimation}
+                  initial="initial"
+                  whileInView="animate"
+                  custom={1.4}
+                >
                   <Cv />
-                </div>
+                </motion.div>
               </div>
             </div>
-            <img
+            <motion.img
+              variants={fadeInAnimation}
+              initial="initial"
+              whileInView="animate"
+              // viewport={{ once: true }}
+              custom={0.4}
               src={Me}
               alt="foto of myself"
               className="xs:w-[80%] sm:w-[60%] md:w-[40%] rounded-3xl mx-auto lg:mt-[-20%] lg:w-[60%]"
